b967411a1ad13834f1665f0eb4e33e5d439f3520
Spencer Oberstadt <soberstadt@gmail.com>
2012-09-30 22:01:36 -0500

    add message boxes

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,46 @@
+this.swagger = "off"
+
+var twitter_options = { 
+    consumerKey: 'DZqGJ4FptGhFcrkhCNWJWw',
+    consumerSecret: 'DxAND4An72rftPDMq6BYFV3sq9AwZi9yDuWx9rJTA',
+    callbackUrl: 'http://www.256design.com/swag' };
+
 this.switchClick = function () {
-  $(this).toggleClass("checked")
+  if(window.clickable !== false) {
+    window.clickable = false
+
+    $(this).toggleClass("checked")
+
+    if(window.swagger == "off") {
+      window.swagger = "on"
+      if(window.twitter || window.facebook)
+        $('#message_box_post').removeClass('hidden_top')
+      else {
+        setTimeout(function() { window.clickable = true },500)
+        $('#message_box_login').removeClass('hidden_top')
+      }
+    }
+    else {
+      window.swagger = "off"
+      setTimeout(function() { window.clickable = true },500)
+      $('#message_box_login').addClass('hidden_top')
+    }
+  }
+}
+
+function hideLoginBox () {
+  $('#message_box_login').addClass('hidden_top')
+  window.clickable = true
+}
+
+this.twitterClick = function () {
+  hideLoginBox()
+}
+
+this.facebookClick = function () {
+  hideLoginBox()
 }
 
 $('#swag_switch').on("mousedown", switchClick)
+$('.twitter').click(twitterClick)
+$('.facebook').click(facebookClick)
